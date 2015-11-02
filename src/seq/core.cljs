958230dfc8b43dafdfa1c73dcf1d6a02cd1e3a14
Mikkel Gravgaard <mikkel@klokke.dk>
2015-11-02 23:40:19 +0100

    clean up

--- src/seq/core.cljs
+++ src/seq/core.cljs
@@ -22,20 +22,13 @@
 
 (set! (.. gain -gain -value) 0.3)
 
-(defonce my-seq [0 3 6 10 12])
-
 (def bpm 140)
 
 (def secs-per-tick (/ (/ 1 (/ bpm 60)) 4))
 
-(defonce state (atom {:time 0
-                      :beat 0
-                      :steps [0 3 6 10 12]
-                      :notes []}))
-
-
 (defn beep
   ([f]
+
     (beep f 0.05))
   ([f t]
     (beep f (.-currentTime context) t))
@@ -46,78 +39,27 @@
      (.start osc start)
      (.stop osc (+ start t)))))
 
-
-
-(defn update-time! []
-  (let [now (.-currentTime context)]
-    (let [{:keys [beat time]} @state
-          next-time (+ time secs-per-tick)]
-      (let [delta-beat (-> (- now time)
-                          (/ secs-per-tick)
-                          (int))]
-        (when (> delta-beat 0)
-          (swap! state assoc :time next-time :beat (+ beat delta-beat)))))))
-
-(defonce update-time-cb (js/setInterval update-time! 100))
-
-(defn debug [a]
-  (prn "debug" a)
-  a)
-
-
-(defn play! []
-  (let [{:keys [beat time notes]} @state
-        play-notes (->> (map #(- % beat) notes)
-                        (debug)
-                        (map #(* secs-per-tick %))
-                        (take-while #(< % 0.15))
-                        (map #(+ time %)))]
-    (when (not (empty? play-notes))
-      (doseq [n play-notes]
-        (beep 400 n 0.05))
-      (swap! state assoc :notes (drop (count play-notes) notes)))))
-
-#_(defonce play-cb (js/setInterval play! 100))
-
-
-(defn update-sequence! []
-  (let [{:keys [beat time steps]} @state
-        iteration (-> (/ beat 16)
-                      (int))
-        new-notes (->> (concat steps (map #(+ 16 %) steps))
-                   (map #(+ % (* 16 iteration)))
-                   (filter #(> % beat))
-                   (map #(- % beat))
-                   (map #(* secs-per-tick %))
-                   (take-while #(< % 0.75))
-                   (map #(+ time %)))]
-
-    (doseq [n new-notes]
-      #_(beep 400 n 0.05))
-    #_(swap! state  (fn [{:keys[notes] :as state}]
-                    (debug (assoc state :notes (concat notes new-notes)))))))
-
 (defn update-sequence2! [beat time steps]
   (let [now (.-currentTime context)
-        _ (prn "diff" (- now time))
         new-notes (->> steps
                        (map #(- % beat))
                        (map #(* secs-per-tick %))
                        (map #(+ time %))
-                       (take-while #(< % (+ now 0.75)))
-                       (debug))]
+                       (take-while #(< % (+ now 0.75))))]
 
     (doseq [n new-notes]
       (beep 400 n 0.05))
-    (let [c (count new-notes)
+    (let [diff (- now time)
+          c (max (int (/ diff secs-per-tick)) (count new-notes))
+
+          _ (prn "diff" diff)
           beat' (+ c beat)
           time' (+ (* secs-per-tick c) time)]
-      (js/setTimeout #(update-sequence2! beat' time' (drop c steps)) 500))
+      (js/setTimeout #(update-sequence2! beat' time' (drop (count new-notes) steps)) 500))
 
     #_(swap! state  (fn [{:keys[notes] :as state}]
                       (debug (assoc state :notes (concat notes new-notes)))))))
 
-(defonce update-cb (js/setInterval update-sequence! 500))
 
 (def s (atom [0 3 6 10 12]))
 
