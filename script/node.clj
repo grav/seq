58a48daa17ce5d68f5509d8c80f58de5ef5410ed
Mikkel Gravgaard <mikkel@klokke.dk>
2017-01-26 23:49:16 +0100

    make nodejs script work again

--- script/node.clj
+++ script/node.clj
@@ -1,6 +1,6 @@
 (require 'cljs.build.api)
 
-(cljs.build.api/build "src"
+(cljs.build.api/build "src-nodejs"
                       {:main 'seq.nodejs
                        :output-to "main.js"
                        :target :nodejs})
